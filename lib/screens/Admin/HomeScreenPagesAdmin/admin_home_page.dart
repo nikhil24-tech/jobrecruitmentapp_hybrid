09a428990fba5f6e807e655f64d0decacb7a1b03
shamlisampla <shamlisampla@gmail.com>
2022-07-26 01:26:58 -0400

    admin pages

---
+++ lib/screens/Admin/HomeScreenPagesAdmin/admin_home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../../../constants/style.dart';
+import '../../../widgets/job_listing_widget.dart';
+
+class AdminHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 23),
+      width: double.maxFinite,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 12),
+          Text('JobKart', style: kSmallLogoTextStyle),
+          SizedBox(height: 17),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Latest Jobs', style: kHeading2BoldStyle),
+          ),
+          SizedBox(height: 12),
+          JobListingWidget(),
+          SizedBox(height: 12),
+          JobListingWidget(),
+          SizedBox(height: 12),
+          JobListingWidget(),
+        ],
+      ),
+    );
+  }
+}
